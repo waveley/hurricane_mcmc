7dd62c48a50f722a31a4b151e6c7c203a189dc69
Waveley Qiu <waveley.qiu@gmail.com>
2022-05-05 15:44:43 -0400

    loading split data file uploaded

---
+++ shared_code/load_mcmc.R
@@ -0,0 +1,19 @@
+############
+#
+# Loading Split Data
+#
+###########
+
+load("mcmc_split1.RData")
+load("mcmc_split2.RData")
+load("mcmc_split3.RData")
+load("mcmc_split4.RData")
+load("mcmc_split5.RData")
+
+
+B_list <- c(res1[[1]], res2[[1]], res3[[1]], res4[[1]], res5[[1]])
+mu_list <- c(res1[[2]], res2[[2]], res3[[2]], res4[[2]], res5[[2]])
+sigmasq_list <- c(res1[[3]], res2[[3]], res3[[3]], res4[[3]], res5[[3]])
+big_sigma_list <- c(res1[[4]], res2[[4]], res3[[4]], res4[[4]], res5[[4]])
+
+res_list <- list(B_list, mu_list, sigmasq_list, big_sigma_list)
